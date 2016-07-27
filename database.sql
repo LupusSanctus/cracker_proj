5a10c4fdb08cafed1c88763196590d79029c8c4b
Настя <radagast@localhost.localdomain>
2016-07-27 18:18:18 +0300

    Added data into database
    Corrected database structure

--- database.sql
+++ database.sql
@@ -20,7 +20,7 @@ CREATE TABLE params
     offer_id    INT          NOT NULL,   
     attr_id     INT          NOT NULL, 
     attr_value  VARCHAR(100), 
-    CONSTRAINT PK_param          PRIMARY KEY  (offer_id, attr_id)
+    CONSTRAINT PK_param          PRIMARY KEY  (offer_id, attr_id),
     CONSTRAINT FK_OfferId_Params FOREIGN KEY  (offer_id)  REFERENCES offer (offer_id), 
     CONSTRAINT FK_AttrId_Params  FOREIGN KEY  (attr_id)   REFERENCES offer_attributes (attr_id) 
 );
@@ -33,9 +33,9 @@ CREATE TABLE usr
 
 CREATE TABLE user_offer
 (
-    usr_name  VARCHAR(100) NOT NULL,
-    offer_id   INT          NOT NULL,
-    CONSTRAINT PK_UserOffer PRIMARY KEY  (usr_name, offer_id),
-    CONSTRAINT FK_OfferId_Offer FOREIGN KEY  (offer_id)  REFERENCES offer (offer_id), 
-    CONSTRAINT FK_UserName_Name FOREIGN KEY  (usr_name)  REFERENCES usr   (usr_name) 
+    usr_id    INT NOT NULL,
+    offer_id  INT NOT NULL,
+    PRIMARY KEY  (usr_id, offer_id),
+    CONSTRAINT FK_OfferId_Offer FOREIGN KEY  (offer_id) REFERENCES offer (offer_id), 
+    CONSTRAINT FK_UserName_Name FOREIGN KEY  (usr_id)   REFERENCES usr   (usr_id) 
 );
